29898c71b9e5e1987e669285723b37757bdb9429
Justus Nolte <j.nolte@zeppelin-university.net>
2024-08-14 16:11:40 +0200

    Airbnb clone

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+gem 'font-awesome-sass', '~> 5.15'
+
+gem "simple_form"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.4"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Modern Loft in New York City with city view',
+  address: '450 West 42nd Street, New York, NY 10036',
+  description: 'State of the art modern loft with extra space, modern, kitchen, king-size-bed, huge living room andspacious kitchen. Apparently no pets allowed.',
+  price_per_night: 250,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Cozy Studio in the Heart of Paris',
+  address: '25 Rue Montorgueil, Paris, France',
+  description: 'A charming and cozy studio located in the heart of Paris. Perfect for a romantic getaway, with easy access to the metro and all the major attractions.',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Beachfront Villa in Malibu',
+  address: '30000 Pacific Coast Highway, Malibu, CA 90265',
+  description: 'Relax and unwind in this luxurious beachfront villa with breathtaking ocean views. The villa features 3 bedrooms, a private pool, and direct access to the beach.',
+  price_per_night: 500,
+  number_of_guests: 6
+)
