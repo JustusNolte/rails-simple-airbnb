d301995633310a9b08d370aaeccc249b73f97d60
Justus Nolte <j.nolte@zeppelin-university.net>
2024-08-14 17:29:35 +0200

    done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,10 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @flats = Flat.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
+    else
+      @flats = Flat.all
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,17 @@ Flat.create!(
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267'
 )
 
 Flat.create!(
   name: 'Modern Loft in New York City with city view',
   address: '450 West 42nd Street, New York, NY 10036',
-  description: 'State of the art modern loft with extra space, modern, kitchen, king-size-bed, huge living room andspacious kitchen. Apparently no pets allowed.',
+  description: 'State-of-the-art modern loft with extra space, modern kitchen, king-size bed, huge living room, and spacious kitchen. No pets allowed.',
   price_per_night: 250,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1502672023488-70e25813eb80'
 )
 
 Flat.create!(
@@ -28,7 +30,8 @@ Flat.create!(
   address: '25 Rue Montorgueil, Paris, France',
   description: 'A charming and cozy studio located in the heart of Paris. Perfect for a romantic getaway, with easy access to the metro and all the major attractions.',
   price_per_night: 120,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb'
 )
 
 Flat.create!(
@@ -36,5 +39,6 @@ Flat.create!(
   address: '30000 Pacific Coast Highway, Malibu, CA 90265',
   description: 'Relax and unwind in this luxurious beachfront villa with breathtaking ocean views. The villa features 3 bedrooms, a private pool, and direct access to the beach.',
   price_per_night: 500,
-  number_of_guests: 6
+  number_of_guests: 6,
+  picture_url: 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29'
 )
